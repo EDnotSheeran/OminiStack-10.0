b205a2a9d3e0aeb35ec2190425f6d323b1774009
EDnotSheeran <juninho.tab3@gmail.com>
2020-02-25 00:16:37 -0300

    *

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,10 @@ const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
+routes.get('/',(req,res) => {
+    return res.send("<h1>Hello World</h1>");
+});
+
 routes.post('/devs', DevController.store);
 routes.get('/devs', DevController.index);
 routes.put('/devs', DevController.update);
